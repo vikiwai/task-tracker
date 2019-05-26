df7b9ecf2ed5af6d48fcd4080ddc86d279225ac6
Victoria Bunyaeva <avikiwai@gmail.com>
2019-05-26 20:36:52 +0300

    Add class 'Task' for presenting data

---
+++ task-tracker/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  task-tracker
+//
+//  Created by vikiwai on 26/05/2019.
+//  Copyright © 2019 Victoria Bunyaeva. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var headline: String = ""
+    var checked: Bool = false
+}
