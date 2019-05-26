dad5b45c848ca0157c9af7967dea1c895325893d
Victoria Bunyaeva <avikiwai@gmail.com>
2019-05-26 22:27:54 +0300

    Add class 'TaskList'

---
+++ task-tracker/TaskList.swift
@@ -0,0 +1,21 @@
+//
+//  TaskList.swift
+//  task-tracker
+//
+//  Created by vikiwai on 26/05/2019.
+//  Copyright © 2019 Victoria Bunyaeva. All rights reserved.
+//
+
+import Foundation
+
+class TaskList {
+    
+    var todo: Array<Task> = []
+    
+    init() {
+        let item = Task()
+        item.headline = "Test"
+        
+        todo.append(item)
+    }
+}
