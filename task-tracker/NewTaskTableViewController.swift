1ddf0e7593fdbb5e9f48fbe0f5d05ee24a83056f
Victoria Bunyaeva <avikiwai@gmail.com>
2019-05-27 16:00:41 +0300

    Edit event handler

--- task-tracker/NewTaskTableViewController.swift
+++ task-tracker/NewTaskTableViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class NewTaskTableViewController: UIViewController {
 
+    @IBOutlet weak var taskHeadline: UITextField!
+    @IBOutlet weak var taskData: UITextField!
+    @IBOutlet weak var taskPriority: UITextField!
+    @IBOutlet weak var taskNotes: UITextView!
+    
     @IBAction func complete(_ sender: Any) {
-        navigationController?.popViewController(animated: true)
+        if taskHeadline.text!.isEmpty || taskData.text!.isEmpty  {
+            let alertController = UIAlertController(title: "Not entered all the data", message: "Fields 'headline' and 'date' must be filled", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Okay", style: UIAlertAction.Style.default) {
+                UIAlertAction in NSLog("OK")
+            }
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        taskHeadline.delegate = self
+        taskData.delegate = self
+        taskPriority.delegate = self
+    }
+}
+
+extension NewTaskTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        taskHeadline.resignFirstResponder()
+        taskData.resignFirstResponder()
+        taskPriority.resignFirstResponder()
+        
+        return false
     }
 }
