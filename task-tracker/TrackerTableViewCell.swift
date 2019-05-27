d4b8145ed203157b35672d3d3f9a88468838274b
Victoria Bunyaeva <avikiwai@gmail.com>
2019-05-27 22:46:13 +0300

    Add class for table cell

---
+++ task-tracker/TrackerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrackerTableViewCell.swift
+//  task-tracker
+//
+//  Created by vikiwai on 27/05/2019.
+//  Copyright © 2019 Victoria Bunyaeva. All rights reserved.
+//
+
+import UIKit
+
+class TrackerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
